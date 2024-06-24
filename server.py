b71d09f1edec76274e8734e6cb37a217bd947b87
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-06-24 10:58:02 +0300

    made the nginx handle the different endpoints

--- server.py
+++ server.py
@@ -1,23 +1,12 @@
-import os
 from fastapi import FastAPI
-from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory=os.path.join("public", "static")), name="static")
-
-@app.get("/", response_class=HTMLResponse)
+@app.get("/api")
 def main():
-    html_file_path = os.path.join("public", "index.html")
-    with open(html_file_path, 'r') as html_file:
-        return html_file.read()
+    return {"message": "all is good"}
 
-@app.get("/img")
-def get_img():
-    file_path = os.path.join("public", "static", "img.jpg")
-    return FileResponse(file_path)
-    
-@app.get("/redirect")
-def redirect_to_root():
-    return RedirectResponse(url="/")
+
+@app.get("/test")
+def test_endpoint():
+    return {"message": "just a test endpoint"}
